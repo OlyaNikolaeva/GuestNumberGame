7b3b069de5b96ec69b9ef69945e7017bbb5837ed
olya_nick <olya17nikolbas@gmail.com>
2024-03-12 23:04:45 +0300

    Добавлен новый класс AutomaticSettingsInput

---
+++ GuestNumberGame/AutomaticSettingsInput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestNumberGame
+{
+    public class AutomaticSettingsInput : ISettingsInput
+    {
+        private readonly Random _random = new Random();
+
+        public (int, int) GetRange()
+        {
+            return (_random.Next(1, 10), _random.Next(10, 30));
+        }
+
+        public int GetNumberOfAttempt()
+        {
+            return _random.Next(1, 5);
+        }
+    }
+}
